434d7286e64ed817aef16f11c1c7f442a9ab236b
Pedro-Coutinho <pedro_coutinho18@outlook.com>
2020-03-26 10:08:34 +0000

    Delete letter from string

---
+++ Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração das variáveis
+            string str, temp;
+            char c;
+
+            // Pedir string ao utilizador
+            Console.WriteLine("Insira uma string: ");
+            str = Console.ReadLine();
+            // Pedir carater c ao utilizador
+            Console.WriteLine("Insira um carater: ");
+            temp = Console.ReadLine();
+            c = char.Parse(temp); 
+            //Percorrer string
+            foreach(char cDaStr in str)
+            {
+                //Imprimir caracter da string, excepto se for carater c
+                if ( cDaStr != c)
+                {
+                    Console.Write(cDaStr);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
